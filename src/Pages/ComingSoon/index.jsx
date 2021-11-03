106aa41a6bc36d8c7e4182a7505381311328d287
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-11-03 22:30:39 +0330

    Pass props to gear component

--- src/Pages/ComingSoon/index.jsx
+++ src/Pages/ComingSoon/index.jsx
@@ -1,12 +1,13 @@
 import Countdown from "./components/Countdown";
 import Gear from "./components/Gear";
+import gearImage from "./images/gear.svg";
 import { Main } from "./view";
 
 export default function ComingSoon() {
   return (
     <Main>
       <Countdown />
-      <Gear />
+      <Gear src={gearImage} alt="Spinning Gear" />
     </Main>
   );
 }
