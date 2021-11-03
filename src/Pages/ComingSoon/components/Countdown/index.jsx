66ff1eb1c5d69f1e9ede5f353099a435ca72d66b
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-11-03 20:32:18 +0330

    Fix eslint warning

--- src/Pages/ComingSoon/components/Countdown/index.jsx
+++ src/Pages/ComingSoon/components/Countdown/index.jsx
@@ -5,6 +5,18 @@ export default function Countdown({ futureDate }) {
   const [timer, setTimer] = useState({ days: 0, hours: 0, mins: 0, secs: 0 });
 
   useEffect(() => {
+    const startTimer = () => {
+      const future = moment(futureDate);
+      const now = moment();
+      const clockDuration = duration(future.diff(now));
+
+      const days = Math.floor(clockDuration.asDays());
+      const hours = clockDuration.hours();
+      const mins = clockDuration.minutes();
+      const secs = clockDuration.seconds();
+      setTimer({ days, hours, mins, secs });
+    };
+
     startTimer();
     const timerInterval = setInterval(() => {
       startTimer();
@@ -12,26 +24,14 @@ export default function Countdown({ futureDate }) {
     return () => {
       clearInterval(timerInterval);
     };
-  }, []);
-
-  const startTimer = () => {
-    const future = moment(futureDate);
-    const now = moment();
-    const clockDuration = duration(future.diff(now));
-
-    const days = Math.floor(clockDuration.asDays());
-    const hours = clockDuration.hours();
-    const mins = clockDuration.minutes();
-    const secs = clockDuration.seconds();
-    setTimer({ days, hours, mins, secs });
-  };
+  }, [futureDate]);
 
   return (
     <div>
-      {Object.keys(timer).map((key) => (
-        <div>
-          <span>{timer[key]}</span>
-          <span>{key.toUpperCase()}</span>
+      {Object.keys(timer).map((objKey, index) => (
+        <div key={index}>
+          <span>{timer[objKey]}</span>
+          <span>{objKey.toUpperCase()}</span>
         </div>
       ))}
     </div>
