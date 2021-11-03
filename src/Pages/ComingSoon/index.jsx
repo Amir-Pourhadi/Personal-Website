4867906bd7077ec9e5a93f31b689c38f4874192b
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-11-03 22:27:00 +0330

    Render Gear Component

--- src/Pages/ComingSoon/index.jsx
+++ src/Pages/ComingSoon/index.jsx
@@ -1,10 +1,12 @@
 import Countdown from "./components/Countdown";
+import Gear from "./components/Gear";
 import { Main } from "./view";
 
 export default function ComingSoon() {
   return (
     <Main>
       <Countdown />
+      <Gear />
     </Main>
   );
 }
