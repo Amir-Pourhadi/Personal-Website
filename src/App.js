a46b53a4d1dc9e244c55db30e9b64809c5565af1
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-11-03 11:57:58 +0330

    render coming soon page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Switch } from "react-router";
+import ComingSoon from "ComingSoon";
+import { Redirect, Route, Switch } from "react-router";
 import { ThemeProvider } from "styled-components";
+import Theme from "Theme";
 import { GlobalStyle } from "./GlobalStyle";
 
 export default function App() {
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={Theme}>
       <div className="container-fluid p-0">
         <GlobalStyle />
         <Switch>
-          
+          <Route path="/coming-soon" component={ComingSoon} />
+          <Redirect from="/" to="/coming-soon" />
         </Switch>
       </div>
     </ThemeProvider>
