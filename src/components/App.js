5c4fa8ae7042e6b87520c8dd50754fa90af7b60c
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-11-03 11:03:18 +0330

    Add theme and route

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Switch } from "react-router";
+import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./GlobalStyle";
 
 export default function App() {
   return (
-    <div className="container-fluid p-0">
-      <GlobalStyle />
-    </div>
+    <ThemeProvider>
+      <div className="container-fluid p-0">
+        <GlobalStyle />
+        <Switch>
+          
+        </Switch>
+      </div>
+    </ThemeProvider>
   );
 }
