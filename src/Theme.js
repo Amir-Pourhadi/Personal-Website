c90dd63b5fbe9ca4f9574457e7151c8989fdcabc
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-11-03 12:08:31 +0330

    Add coming soon theme colors

--- src/Theme.js
+++ src/Theme.js
@@ -1,3 +1,14 @@
-const Theme = {};
+const Theme = {
+  comingSoon: {
+    colors: {
+      green: "#4cedc4",
+      darkGray: "#292f36",
+      white: "#fffffa",
+      black: "#000013",
+      orange: "#fa8334",
+      red: "#ff312e",
+    },
+  },
+};
 
 export default Theme;
