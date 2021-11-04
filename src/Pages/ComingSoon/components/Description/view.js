2bd88c14ba4b0bd7223e00a947d9fc75f1931535
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-11-04 09:47:15 +0330

    Add Description styles

--- src/Pages/ComingSoon/components/Description/view.js
+++ src/Pages/ComingSoon/components/Description/view.js
@@ -1,5 +1,14 @@
 import styled from "styled-components";
 
-export const Container = styled.div({});
+export const Container = styled.div({
+  gridArea: "description",
+  display: "grid",
+  fontSize: 16,
+  fontWeight: 500,
+  maxWidth: 700,
+  gridTemplateColumns: "1fr",
+});
 
-export const Content = styled.p({});
+export const Content = styled.p({
+  grid: "1 / -1 1 / -1",
+});
