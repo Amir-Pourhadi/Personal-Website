79b05eb561e288fc56de45554ce7f0dd1ebe2e30
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-11-04 10:14:34 +0330

    Render Subscribe component

--- src/Pages/ComingSoon/index.jsx
+++ src/Pages/ComingSoon/index.jsx
@@ -1,6 +1,7 @@
 import Countdown from "./components/Countdown";
 import Description from "./components/Description";
 import Gear from "./components/Gear";
+import Subscribe from "./components/Subscribe";
 import Title from "./components/Title";
 import gearImage from "./images/gear.svg";
 import { Main } from "./view";
@@ -12,6 +13,7 @@ export default function ComingSoon() {
       <Gear src={gearImage} alt="Spinning Gear" />
       <Title />
       <Description />
+      <Subscribe placeholder="Enter Email Address" buttonText="Submit" />
     </Main>
   );
 }
