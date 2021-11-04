ce30b4f9b4c72a3d67aba44cb088030fd6cda1a6
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-11-04 17:54:50 +0330

    Import data to main file

--- src/Pages/ComingSoon/index.jsx
+++ src/Pages/ComingSoon/index.jsx
@@ -1,3 +1,4 @@
+import { pages } from "data/db.json";
 import Countdown from "./components/Countdown";
 import Description from "./components/Description";
 import Gear from "./components/Gear";
@@ -7,13 +8,15 @@ import gearImage from "./images/gear.svg";
 import { Main } from "./view";
 
 export default function ComingSoon() {
+  const { countdown, gear, title, description, subscribe } = pages.comingSoon.sections;
+
   return (
     <Main>
-      <Countdown />
-      <Gear src={gearImage} />
-      <Title />
-      <Description />
-      <Subscribe />
+      <Countdown futureDate={countdown.futureDate} />
+      <Gear src={gearImage} alt={gear.alt} />
+      <Title text={title.text} />
+      <Description description={description.text} />
+      <Subscribe placeholder={subscribe.placeholder} buttonText={subscribe.buttonText} />
     </Main>
   );
 }
