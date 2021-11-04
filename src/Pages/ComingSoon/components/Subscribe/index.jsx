8036863630b41c4ed824ba18a9ceb604149a55d6
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-11-04 10:23:52 +0330

    Update Subscribe component

--- src/Pages/ComingSoon/components/Subscribe/index.jsx
+++ src/Pages/ComingSoon/components/Subscribe/index.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 export default function Subscribe({ placeholder, buttonText }) {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const handleChange = (event) => {
+    setEmail(event.target.value.trim());
+  };
+
   return (
-    <form>
-      <input type="email" name="email" placeholder={placeholder} />
+    <form onSubmit={handleSubmit}>
+      <input type="email" name="email" placeholder={placeholder} value={email} onChange={handleChange} />
       <button type="submit">{buttonText}</button>
     </form>
   );
