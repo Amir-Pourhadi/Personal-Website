399ff19d98b2a45d596ec55434706f693c994677
Amir Pourhadi <AmirPourhadi@outlook.com>
2021-12-11 19:19:59 +0330

    Render Notification

--- src/Pages/ComingSoon/index.jsx
+++ src/Pages/ComingSoon/index.jsx
@@ -1,18 +1,37 @@
 import { pages } from "data/db.json";
+import { useState } from "react";
 import { Countdown, Description, Gear, Subscribe, Title } from "./components";
 import gearImage from "./images/gear.svg";
 import { Main } from "./view";
 
 export default function ComingSoon() {
-  const { countdown, gear, title, description, subscribe } = pages.comingSoon.sections;
+  const [notify, setNotify] = useState({ visible: false, level: "error", icon: null });
+
+  const showNotification = (level, icon) => {
+    setNotify({ visible: true, level, icon });
+    setTimeout(() => setNotify({ visible: false, level: "error", icon: null }), 5000);
+  };
+
+  const { countdown, gear, title, description, subscribe, notification } = pages.comingSoon.sections;
 
   return (
     <Main>
       <Countdown futureDate={countdown.futureDate} />
       <Gear src={gearImage} alt={gear.alt} />
       <Title text={title.text} />
-      <Description text={description.text} />
-      <Subscribe placeholder={subscribe.placeholder} buttonText={subscribe.buttonText} />
+      <Description
+        text={description.text}
+        message={notification[notify.level].message}
+        icon={notify.icon}
+        alt={notification[notify.level].alt}
+        visible={notify.visible}
+        bgColor={notification[notify.level].bgColor}
+      />
+      <Subscribe
+        placeholder={subscribe.placeholder}
+        buttonText={subscribe.buttonText}
+        showNotification={showNotification}
+      />
     </Main>
   );
 }
