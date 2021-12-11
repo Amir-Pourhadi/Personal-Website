fbce83c9fe3bc7b2723c1625c4cbad70e1c21089
Amir Pourhadi <AmirPourhadi@outlook.com>
2021-12-11 19:21:16 +0330

    Render Notification

--- src/Pages/ComingSoon/components/Description/index.jsx
+++ src/Pages/ComingSoon/components/Description/index.jsx
@@ -1,9 +1,11 @@
 import { Container, Content } from "./view";
+import Notification from "../Notification";
 
-export default function Description({ text }) {
+export default function Description({ text, message, icon, alt, visible, bgColor }) {
   return (
     <Container>
       <Content>{text}</Content>
+      <Notification message={message} icon={icon} alt={alt} visible={visible} bgColor={bgColor} />
     </Container>
   );
 }
